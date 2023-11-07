b6f18cd56b5eafb1e83a4ad46d3ec98bc2bf6b59
ikhsannh <nashkispace@gmail.com>
2023-11-07 07:20:22 +0700

    hello starterKit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className='flex-col lg:flex justify-around'>
+        Hello from up ikhsannh dot com
+      </div>
+    </main>
+  )
+}
